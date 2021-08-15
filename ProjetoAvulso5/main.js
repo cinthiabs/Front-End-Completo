f940bbf61257cc367cc0e38cabe8a468a183a6b2
cinthiabs <cinthiabarbosa8d@outlook.com>
2021-08-15 20:15:13 -0300

    Curso

---
+++ ProjetoAvulso5/main.js
@@ -0,0 +1,23 @@
+/* Mostrar menu*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+  if (navToggle){
+      navToggle.addEventListener('click',()=>{
+          navMenu.classList.add('show-menu')
+      })
+  }  
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}  
+
+/*Remove Menu*/
+const navLink = document.querySelectorAll('.nav_link')
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
